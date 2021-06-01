a9b2aa74c3bda5e304169812b790368aa7d5ec1f
MHWillard <mhwillard@gmail.com>
2021-06-01 06:53:23 -0400

    working on React props

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,52 @@
+import React from 'react'
 import './App.css';
 import Start from './components/Start';
+import axios from "axios";
 
 //state must be set here and passed down into Start component as a prop
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-      </header>
-      <body>
-        <Start />
-      </body>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        data: '',
+        test: 'fart'
+    };
+
+    this.reArm = this.reArm.bind(this);
+}
+
+reArm() {
+  this.setState({
+      data: "test"
+   });
+}
+
+componentDidMount() {
+  axios.get('http://localhost:5000').then(res => {
+      //console.log(res.data.person);
+      let dataObject = res.data.person;
+      console.log("dataObject" + dataObject);
+      let [firstname, lastname, age, problem, business, goal, moneygoal] = dataObject
+      console.log("name" + firstname)
+      console.log("object name" + dataObject.firstname)
+      console.log("object data firstname" + res.data.person.firstname)
+      //spread person object
+      //assign to each property in state
+   });
+}
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+        </header>
+        <body>
+          <Start data={this.state.data} test={this.state.test} reArm={this.reArm} />
+        </body>
+      </div>
+    );
+  }
 }
 
 export default App;
--- client/src/components/Start.jsx
+++ client/src/components/Start.jsx
@@ -1,45 +1,19 @@
 import React from 'react'
-import axios from "axios";
+
 
 class Start extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: '',
-            test: 'fart'
-        };
 
-        this.reArm = this.reArm.bind(this);
-    }
-
-    componentDidMount() {
-       axios.get('http://localhost:5000').then(res => {
-           //console.log(res.data.person);
-           let dataObject = res.data.person;
-           console.log("dataObject" + dataObject);
-           let [firstname, lastname, age, problem, business, goal, moneygoal] = dataObject
-           console.log("name" + firstname)
-           console.log("object name" + dataObject.firstname)
-           console.log("object data firstname" + res.data.person.firstname)
-           //spread person object
-           //assign to each property in state
-        });
-    }
 
     //this needs to take state and loop it back up into App component
-    reArm() {
-        this.setState({
-            data: "test"
-         });
-    }
+
 
     render() {
         return(
             <div>
                 <p>Start component</p>
-                <p>Data: {this.state.data}</p>
-                <p>Test: {this.state.test}</p>
-                <form onSubmit={this.reArm}>
+                <p>Data: {this.props.data}</p>
+                <p>Test: {this.props.test}</p>
+                <form onSubmit={this.props.reArm}>
                     <button type="submit">Reload</button>
                 </form>
             </div>
